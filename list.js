c4a9ccf77d849184eccfad19deeba859f3f36a3c
VasylKhrapko <khrapko2002@gmail.com>
2022-03-01 16:28:13 +0200

    Added .append() method to add nodes to the end of the list
    The method takes a character - the value of the node to be added to
    the end of the list and creates a new node object
    and places it at the tail of the list

--- list.js
+++ list.js
@@ -27,4 +27,16 @@ class List {
 
     return length;
   }
+
+  append(element) {
+    const newNode = new Node(element);
+    if (this.length() === 0) {
+      this.#head = newNode;
+      this.#tail = this.#head;
+      return;
+    }
+    newNode.previous = this.#tail;
+    this.#tail.next = newNode;
+    this.#tail = newNode;
+  }
 }
