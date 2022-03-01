17ab8c4f142f21c1de4a99527e7372c44061a8b9
VasylKhrapko <khrapko2002@gmail.com>
2022-03-01 19:41:31 +0200

    Added tests for .insert() method

--- list.test.js
+++ list.test.js
@@ -87,3 +87,65 @@ describe('Testing the .get() method', () => {
     expect(() => list.get(5)).toThrow('Wrong position specified');
   });
 });
+
+describe('Testing the .insert() method', () => {
+  test('Should insert new node with value equal to "1" to the position with index 1 in the list', () => {
+    const list = new List();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    list.insert('1', 1);
+
+    expect(list.length()).toBe(4);
+    expect(list.get(1)).toBe('1');
+  });
+
+  test('Should insert new node with value equal to 1 to the position with index 0 in the empty list', () => {
+    const list = new List();
+
+    list.insert('1', 0);
+
+    expect(list.length()).toBe(1);
+    expect(list.get(0)).toBe('1');
+  });
+
+  test('Should insert new node with value equal to 1 to the position with index 3 (in the tail) of the list', () => {
+    const list = new List();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    list.insert('1', 3);
+
+    expect(list.length()).toBe(4);
+    expect(list.get(3)).toBe('1');
+  });
+
+  test('Should throw an error when trying to insert a node with the wrong value (string specified as a value has length not equal to 1)', () => {
+    const list = new List();
+
+    expect(() =>
+      list
+        .insert('11', 0)
+        .toThrow(
+          'The specified value must be a string with a length equal to one'
+        )
+    );
+  });
+
+  test('Should throw an error when trying to insert node at negative index in a list', () => {
+    const list = new List();
+
+    expect(() => list.insert('1', -1)).toThrow('Wrong position specified');
+  });
+
+  test('Should throw an error when trying to insert a node at an index greater than the length of the list', () => {
+    const list = new List();
+    list.append('a');
+    list.append('b');
+    list.append('c');
+
+    expect(() => list.insert('1', 5)).toThrow('Wrong position specified');
+  });
+});
